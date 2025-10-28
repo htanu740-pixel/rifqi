976344cb9c0b58039b28f8dc3c150ee62794cef4
htanu740-pixel <htanu740@gmail.com>
2025-10-28 11:27:45 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // 1. Pilih tombo; berdasarkan ID-nya 
-const tombolSapa = document.getElemenByld(sapaButton');
+const tombolSapa = document.getElemenByld('sapaButton');
 //2 Tambahkan 'event listener' untuk aksi 'click'
 tombolSapa.addEventListener('click',function() {
     //3. Jalankan kode ini ketika tombol di-klik
