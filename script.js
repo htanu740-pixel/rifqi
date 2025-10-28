451ca9bb56b514272ef691a2b73be70a2b5d64b4
htanu740-pixel <htanu740@gmail.com>
2025-10-28 11:29:59 +0700

    Update script.js

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ const tombolSapa = document.getElemenByld('sapaButton');
 //2 Tambahkan 'event listener' untuk aksi 'click'
 tombolSapa.addEventListener('click',function() {
     //3. Jalankan kode ini ketika tombol di-klik
-    alert('halo! Terima kasih sudah berkunjung!');
+    alert('Halo! Terima kasih sudah berkunjung!');
 });
